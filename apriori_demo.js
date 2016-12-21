9a306937624e8d1b0f00c93aa344d060571196fd
IS-CITE <chaloemphon.sir@dpu.ac.th>
2016-12-21 10:34:56 +0700

    Add files via upload

--- apriori_demo.js
+++ apriori_demo.js
@@ -4,9 +4,17 @@ let transactions = {
 	'003': ['ขนมปัง', 'เบียร์', 'ผ้าอ้อม', 'ไข่'],
 	'004': ['เบียร์', 'ไข่']
 };
+let support_count = 0.5 * Object.keys(transactions).length;
 let items = [];
 for(let id in transactions) {
 	for(let item of transactions[id]) {
 		if(!items.includes(item)) items.push(item);
 	}
 }
+let k1_itemset = items.filter((e)=>{
+	let count = 0;
+	for(let id in transactions) {
+		if(transactions[id].includes(e)) count++;
+	}
+	return count >= support_count;
+});
