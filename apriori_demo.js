3a28e3745f5d53a699e30ae532283137b3e392ee
IS-CITE <chaloemphon.sir@dpu.ac.th>
2016-12-31 05:10:54 +0700

    Add files via upload

--- apriori_demo.js
+++ apriori_demo.js
@@ -1,20 +1,67 @@
-let transactions = {
-	'001': ['ขนมปัง', 'ผ้าอ้อม', 'นม'],
-	'002': ['เบียร์', 'ผ้าอ้อม', 'ไข่'],
-	'003': ['ขนมปัง', 'เบียร์', 'ผ้าอ้อม', 'ไข่'],
-	'004': ['เบียร์', 'ไข่']
+const transaction = [
+	['ขนมปัง', 'ผ้าอ้อม', 'นม'],
+	['เบียร์', 'ผ้าอ้อม', 'ไข่'],
+	['ขนมปัง', 'เบียร์', 'ผ้าอ้อม', 'ไข่'],
+	['เบียร์', 'ไข่']
+];
+const support = 0.5;
+
+const support_count = support * transaction.length;
+const match = (a, b)=>{
+	for(let i of b) {
+		if(!a.includes(i)) return false;
+	}
+	return true;
 };
-let support_count = 0.5 * Object.keys(transactions).length;
-let items = [];
-for(let id in transactions) {
-	for(let item of transactions[id]) {
-		if(!items.includes(item)) items.push(item);
+const filter_supported = (c_itemset)=>{
+	return c_itemset.filter((e)=>{
+		let count = 0;
+		for(let t of transaction) {
+			if(match(t, e.item)) e.count++;
+		}
+		return e.count >= support_count;
+	});
+};
+let c_itemset = [];
+let f_itemset = [];
+let item = [];
+let size = 1;
+
+for(let t of transaction) {
+	for(let i of t) {
+		if(!item.includes(i)) {
+			item.push(i);
+			c_itemset.push({item: [i], count: 0});
+		}
 	}
 }
-let k1_itemset = items.filter((e)=>{
-	let count = 0;
-	for(let id in transactions) {
-		if(transactions[id].includes(e)) count++;
+f_itemset[size] = filter_supported(c_itemset);
+while(f_itemset[size].length > 1) {
+	c_itemset = [];
+	for(let i = 0, s = size - 1; i < f_itemset[size].length; i++) {
+		let a = f_itemset[size][i].item;
+		for(let j = i + 1; j < f_itemset[size].length; j++) {
+			let b = f_itemset[size][j].item;
+			if(match(a.slice(0, s), b.slice(0, s))) {
+				let tmp = a.concat(b[s]);
+				(()=>{
+					let m = j;
+					for(let k = s - 1; k >= 0; k--) {
+						let c = [...tmp];
+						c.splice(k, 1);
+						while(++m < f_itemset[size].length) {
+							if(match(c, f_itemset[size][m].item)) break;
+						}
+						if(m == f_itemset[size].length) return;
+					}
+					c_itemset.push({item: tmp, count: 0});
+				})();
+			}
+			else {
+				break;
+			}
+		}
 	}
-	return count >= support_count;
-});
+	f_itemset[++size] = filter_supported(c_itemset);
+}
+console.log(JSON.stringify(f_itemset));
